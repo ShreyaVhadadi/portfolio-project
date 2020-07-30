865a079280b06ced6dbd5d3406db7de35448bf9b
shreya <shreyavhadadi@gmail.com>
2020-07-29 23:48:33 -0400

    Added animations to project cards

--- jobs/models.py
+++ jobs/models.py
@@ -2,5 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Job(models.Model):
+    title = models.CharField(max_length=100,default="")
     image = models.ImageField(upload_to='images/')
     summary=models.CharField(max_length=200)
+    url = models.URLField(max_length=200,default="")
