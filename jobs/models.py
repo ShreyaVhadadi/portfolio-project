62cb999a94f940bb4901bd1c36437e000f44d1f7
shreya <shreyavhadadi@gmail.com>
2020-08-10 14:00:03 -0400

    Done with the database

--- jobs/models.py
+++ jobs/models.py
@@ -10,7 +10,7 @@ class Job(models.Model):
 class Contact(models.Model):
         name = models.CharField(max_length=200)
         from_email = models.EmailField()
-        subject = models.CharField(max_length=300)
+        subject = models.CharField(max_length=800)
         message = models.TextField(blank=False)
 
         def __str__(self):
--- jobs/views.py
+++ jobs/views.py
@@ -3,13 +3,15 @@ from .models import Job
 from django.http import HttpResponse
 from .forms import ContactForm
 from .models import Contact
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 # Create your views here.
 def home(request):
     jobs=Job.objects
     return render(request,'jobs/home.html',{'jobs':jobs})
     #return HttpResponse('<h1>Hi</h1>')
 
-def contactView(request):
+'''def contactView(request):
     if request.method == 'GET':
         return render(request, "jobs/success.html", {'form': ContactForm()})
 
@@ -24,10 +26,21 @@ def contactView(request):
             print(Success)
             return redirect ('success')
         except ValueError:
-            return render(request, "jobs/success.html", {'form': ContactForm(),'error':'Bad data passed in. Try again.'})
+            return render(request, "jobs/success.html", {'form': ContactForm(),'error':'Bad data passed in. Try again.'})'''
 
 
 
+def contactView(request):
+    if request.method == 'POST':
+        contact = Contact()
+        contact.name = request.POST['username']
+        contact.from_email = request.POST['email']
+        contact.subject = request.POST['subject']
+        contact.message = request.POST['message']
+        contact.save()
+        return render(request,'jobs/success.html')
+    else:
+        return render(request, "jobs/success.html", {'form': ContactForm(),'error':'Bad data passed in. Try again.'})
 
 
 
