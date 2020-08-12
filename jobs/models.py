9143196b4baec798fdb8fcda727a7f9fd1fc65f5
shreya <shreyavhadadi@gmail.com>
2020-08-12 03:46:19 -0400

    Errors resolved

--- jobs/models.py
+++ jobs/models.py
@@ -10,7 +10,7 @@ class Job(models.Model):
 class Contact(models.Model):
         name = models.CharField(max_length=200)
         from_email = models.EmailField()
-        subject = models.CharField(max_length=800)
+        subject = models.CharField(max_length=400)
         message = models.TextField(blank=False)
 
         def __str__(self):
